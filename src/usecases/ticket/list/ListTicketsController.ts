d5d35b30248c7befa9525175338c195065e9e575
Gabriel <gabrielvieiraguimaraes4@gmail.com>
2024-07-10 17:38:53 -0400

    feat: order with priority

--- src/usecases/ticket/list/ListTicketsController.ts
+++ src/usecases/ticket/list/ListTicketsController.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from "express";
 import { ListTicketsUseCase } from "./ListTicketsUseCase";
 import { getToken } from "../../../services/jwt/GetToken";
+import { TicketPriority } from "../../../entities/ticket/TicketProps";
+import { Ticket } from "../../../entities/ticket/Ticket";
 
 export class ListTicketsController {
   constructor(private listTicketsUseCase: ListTicketsUseCase) {}
@@ -12,32 +14,83 @@ export class ListTicketsController {
       const token = getToken(req);
       const tickets = await this.listTicketsUseCase.execute(
         token,
-        getOrder(order)
+        this.getOrder(order)
       );
-      return res.status(200).json(tickets);
+
+      const ordered = this.filterPriority(tickets);
+
+      return res.status(200).json(ordered);
     } catch (error) {
       if (error instanceof Error) {
         return res.status(400).json({ error: error.message });
       }
     }
   }
-}
 
-function getOrder(order: string) {
-  switch (order) {
-    case "all":
-      return "all";
+  getOrder(order: string) {
+    switch (order) {
+      case "all":
+        return "all";
 
-    case "daily":
-      return "daily";
+      case "daily":
+        return "daily";
 
-    case "delivery":
-      return "delivery";
+      case "delivery":
+        return "delivery";
 
-    case "budget":
-      return "budget";
+      case "budget":
+        return "budget";
 
-    default:
-      throw new Error("category unknow");
+      default:
+        throw new Error("category unknow");
+    }
+  }
+
+  filterPriority(tickets: Ticket[]) {
+    const urgents = tickets.filter(
+      (ticket) => ticket.priority == TicketPriority.urgent
+    );
+
+    const highs = tickets.filter(
+      (ticket) => ticket.priority == TicketPriority.high
+    );
+
+    const mediums = tickets.filter(
+      (ticket) => ticket.priority == TicketPriority.medium
+    );
+
+    const lows = tickets.filter(
+      (ticket) => ticket.priority == TicketPriority.low
+    );
+
+    tickets.filter((ticket, i) => {
+      if (ticket.priority == TicketPriority.urgent) {
+        tickets.splice(i, 1);
+      }
+    });
+
+    tickets.filter((ticket, i) => {
+      if (ticket.priority == TicketPriority.high) {
+        tickets.splice(i, 1);
+      }
+    });
+
+    tickets.filter((ticket, i) => {
+      if (ticket.priority == TicketPriority.medium) {
+        tickets.splice(i, 1);
+      }
+    });
+
+    tickets.filter((ticket, i) => {
+      if (ticket.priority == TicketPriority.low) {
+        tickets.splice(i, 1);
+      }
+    });
+
+    lows.forEach((ticket) => tickets.unshift(ticket));
+    mediums.forEach((ticket) => tickets.unshift(ticket));
+    highs.forEach((ticket) => tickets.unshift(ticket));
+    urgents.forEach((ticket) => tickets.unshift(ticket));
+    return tickets;
   }
 }
