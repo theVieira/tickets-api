36611751ac6d81e91ae16601f1bc15a573771cda
Gabriel <gabrielvieiraguimaraes4@gmail.com>
2024-07-11 09:40:06 -0400

    fix: fix order

--- src/usecases/ticket/list/ListTicketsUseCase.ts
+++ src/usecases/ticket/list/ListTicketsUseCase.ts
@@ -4,6 +4,8 @@ import { config } from "dotenv";
 import { IPayload } from "../../../services/jwt/IPayload";
 import { checkPermission } from "../../../services/checkPermission/CheckPermission";
 import { orderTickets } from "../../../services/utils/OrderTickets";
+import { TicketPriority } from "../../../entities/ticket/TicketProps";
+import { Ticket } from "../../../entities/ticket/Ticket";
 
 config();
 const SECRET = process.env.SECRET_KEY || "";
@@ -26,5 +28,53 @@ export class ListTicketsUseCase {
     }
 
     return orderTickets(openTickets, order);
+  }
+
+  filterPriority(tickets: Ticket[]) {
+    const urgents = tickets.filter(
+      (ticket) => ticket.priority == TicketPriority.urgent
+    );
+
+    const highs = tickets.filter(
+      (ticket) => ticket.priority == TicketPriority.high
+    );
+
+    const mediums = tickets.filter(
+      (ticket) => ticket.priority == TicketPriority.medium
+    );
+
+    const lows = tickets.filter(
+      (ticket) => ticket.priority == TicketPriority.low
+    );
+
+    tickets.forEach((ticket, i) => {
+      if (ticket.priority == TicketPriority.urgent) {
+        tickets.splice(i, 1);
+      }
+    });
+
+    tickets.forEach((ticket, i) => {
+      if (ticket.priority == TicketPriority.high) {
+        tickets.splice(i, 1);
+      }
+    });
+
+    tickets.forEach((ticket, i) => {
+      if (ticket.priority == TicketPriority.medium) {
+        tickets.splice(i, 1);
+      }
+    });
+
+    tickets.forEach((ticket, i) => {
+      if (ticket.priority == TicketPriority.low) {
+        tickets.splice(i, 1);
+      }
+    });
+
+    lows.forEach((ticket) => tickets.unshift(ticket));
+    mediums.forEach((ticket) => tickets.unshift(ticket));
+    highs.forEach((ticket) => tickets.unshift(ticket));
+    urgents.forEach((ticket) => tickets.unshift(ticket));
+    return tickets;
   }
 }
