516224146227cbf97170861e49a9c2b5c96e3325
theVieira <gabrielvieiraguimaraes4@gmail.com>
2024-08-19 14:50:51 -0400

    chore: use order type

--- src/services/utils/OrderTickets.ts
+++ src/services/utils/OrderTickets.ts
@@ -1,64 +1,56 @@
-import { Ticket } from "../../entities/ticket/Ticket";
-import {
-  TicketCategory,
-  TicketPriority,
-} from "../../entities/ticket/TicketProps";
+import { Ticket } from '../../entities/ticket/Ticket'
+import { TicketCategory, TicketPriority } from '../../entities/ticket/TicketProps'
+import { OrderType } from '../../types/OrderType'
 
-export function orderTickets(
-  tickets: Ticket[],
-  order: "daily" | "delivery" | "budget" | "all"
-): Ticket[] {
-  switch (order) {
-    case "all":
-      return orderByPriority(tickets);
+export function orderTickets(tickets: Ticket[], order: OrderType): Ticket[] {
+	switch (order) {
+		case OrderType.all:
+			return orderByPriority(tickets)
 
-    case "daily":
-      const dailys: Ticket[] = getDaily(tickets);
-      return orderByPriority(dailys);
+		case OrderType.daily: {
+			const dailys: Ticket[] = getDaily(tickets)
+			return orderByPriority(dailys)
+		}
 
-    case "delivery":
-      const deliverys: Ticket[] = getDelivery(tickets);
-      return orderByPriority(deliverys);
+		case OrderType.delivery: {
+			const deliverys: Ticket[] = getDelivery(tickets)
+			return orderByPriority(deliverys)
+		}
 
-    case "budget":
-      const budgets: Ticket[] = getBudget(tickets);
-      return orderByPriority(budgets);
-  }
+		case OrderType.budget: {
+			const budgets: Ticket[] = getBudget(tickets)
+			return orderByPriority(budgets)
+		}
+	}
 }
 
 function getDaily(tickets: Ticket[]): Ticket[] {
-  return tickets.filter((ticket) => ticket.category == TicketCategory.daily);
+	return tickets.filter((ticket) => ticket.category == TicketCategory.daily)
 }
 
 function getDelivery(tickets: Ticket[]) {
-  return tickets.filter((ticket) => ticket.category == TicketCategory.delivery);
+	return tickets.filter((ticket) => ticket.category == TicketCategory.delivery)
 }
 
 function getBudget(tickets: Ticket[]) {
-  return tickets.filter((ticket) => ticket.category == TicketCategory.budget);
+	return tickets.filter((ticket) => ticket.category == TicketCategory.budget)
 }
 
 function orderByPriority(tickets: Ticket[]): Ticket[] {
-  const urgent = tickets.filter(
-    (ticket) => ticket.priority == TicketPriority.urgent
-  );
+	const urgent = tickets.filter((ticket) => ticket.priority == TicketPriority.urgent)
 
-  const high = tickets.filter(
-    (ticket) => ticket.priority == TicketPriority.high
-  );
+	const high = tickets.filter((ticket) => ticket.priority == TicketPriority.high)
 
-  const medium = tickets.filter(
-    (ticket) => ticket.priority == TicketPriority.medium
-  );
+	const medium = tickets.filter((ticket) => ticket.priority == TicketPriority.medium)
 
-  const low = tickets.filter((ticket) => ticket.priority == TicketPriority.low);
+	const low = tickets.filter((ticket) => ticket.priority == TicketPriority.low)
 
-  const orderedTickets: Ticket[] = [];
+	const orderedTickets: Ticket[] = []
 
-  low.forEach((ticket) => orderedTickets.unshift(ticket));
-  medium.forEach((ticket) => orderedTickets.unshift(ticket));
-  high.forEach((ticket) => orderedTickets.unshift(ticket));
-  urgent.forEach((ticket) => orderedTickets.unshift(ticket));
+	low.forEach((ticket) => orderedTickets.unshift(ticket))
+	medium.forEach((ticket) => orderedTickets.unshift(ticket))
+	high.forEach((ticket) => orderedTickets.unshift(ticket))
+	urgent.forEach((ticket) => orderedTickets.unshift(ticket))
 
-  return orderedTickets;
+	return orderedTickets
 }
