212e4903dabdad9c74366db3b850516929bf3927
theVieira <gabrielvieiraguimaraes4@gmail.com>
2024-08-19 14:51:22 -0400

    feat: add unique status route

--- src/routes/Ticket.routes.ts
+++ src/routes/Ticket.routes.ts
@@ -1,50 +1,60 @@
-import { Request, Response, Router } from "express";
-import { createTicketController } from "../usecases/ticket/create";
-import { setFinishedController } from "../usecases/ticket/setFinished";
-import { listTicketsController } from "../usecases/ticket/list";
-import { setProgressController } from "../usecases/ticket/setProgress";
-import { reOpenTicketController } from "../usecases/ticket/reopen";
-import { deleteTicketController } from "../usecases/ticket/delete";
-import { editDescriptionController } from "../usecases/ticket/edit";
-import { addNoteController } from "../usecases/ticket/addNote";
-import { openTicketController } from "../usecases/ticket/open";
+import { Request, Response, Router } from 'express'
+import { createTicketController } from '../usecases/ticket/create'
+import { setFinishedController } from '../usecases/ticket/setFinished'
+import { setProgressController } from '../usecases/ticket/setProgress'
+import { reOpenTicketController } from '../usecases/ticket/reopen'
+import { deleteTicketController } from '../usecases/ticket/delete'
+import { editDescriptionController } from '../usecases/ticket/edit'
+import { addNoteController } from '../usecases/ticket/addNote'
+import { openTicketController } from '../usecases/ticket/open'
+import { listOpenController } from '../usecases/ticket/listOpen'
+import { listProgressController } from '../usecases/ticket/listProgress'
+import { listFinishedController } from '../usecases/ticket/listFinished'
 
-const ticketRoutes = Router();
+const ticketRoutes = Router()
 
-ticketRoutes.post("/create", (req: Request, res: Response) => {
-  createTicketController.handle(req, res);
-});
+ticketRoutes.get('/listOpen/:order', (req: Request, res: Response) => {
+	listOpenController.handle(req, res)
+})
 
-ticketRoutes.put("/finished", (req: Request, res: Response) => {
-  setFinishedController.handle(req, res);
-});
+ticketRoutes.get('/listProgress/:order', (req: Request, res: Response) => {
+	listProgressController.handle(req, res)
+})
 
-ticketRoutes.get("/list/:order", (req: Request, res: Response) => {
-  listTicketsController.handle(req, res);
-});
+ticketRoutes.get('/listFinished/:order', (req: Request, res: Response) => {
+	listFinishedController.handle(req, res)
+})
 
-ticketRoutes.put("/progress", (req: Request, res: Response) => {
-  setProgressController.handle(req, res);
-});
+ticketRoutes.post('/create', (req: Request, res: Response) => {
+	createTicketController.handle(req, res)
+})
 
-ticketRoutes.put("/reopen", (req: Request, res: Response) => {
-  reOpenTicketController.handle(req, res);
-});
+ticketRoutes.put('/edit', (req: Request, res: Response) => {
+	editDescriptionController.handle(req, res)
+})
 
-ticketRoutes.delete("/delete", (req: Request, res: Response) => {
-  deleteTicketController.handle(req, res);
-});
+ticketRoutes.put('/finished', (req: Request, res: Response) => {
+	setFinishedController.handle(req, res)
+})
 
-ticketRoutes.put("/edit", (req: Request, res: Response) => {
-  editDescriptionController.handle(req, res);
-});
+ticketRoutes.put('/progress', (req: Request, res: Response) => {
+	setProgressController.handle(req, res)
+})
 
-ticketRoutes.put("/addNote", (req: Request, res: Response) => {
-  addNoteController.handle(req, res);
-});
+ticketRoutes.put('/reopen', (req: Request, res: Response) => {
+	reOpenTicketController.handle(req, res)
+})
 
-ticketRoutes.put("/open", (req: Request, res: Response) => {
-  openTicketController.handle(req, res);
-});
+ticketRoutes.put('/addNote', (req: Request, res: Response) => {
+	addNoteController.handle(req, res)
+})
 
-export { ticketRoutes };
+ticketRoutes.put('/open', (req: Request, res: Response) => {
+	openTicketController.handle(req, res)
+})
+
+ticketRoutes.delete('/delete', (req: Request, res: Response) => {
+	deleteTicketController.handle(req, res)
+})
+
+export { ticketRoutes }
