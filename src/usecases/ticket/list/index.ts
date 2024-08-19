7e94ccc3113ad45d9192b097ada9a7f54ff8f57b
theVieira <gabrielvieiraguimaraes4@gmail.com>
2024-08-19 14:53:43 -0400

    delete: list all ticket route

--- src/usecases/ticket/list/index.ts
+++
@@ -1,9 +0,0 @@
-import { TicketRepository } from "../../../repositories/ticket/TicketRepository";
-import { ListTicketsController } from "./ListTicketsController";
-import { ListTicketsUseCase } from "./ListTicketsUseCase";
-
-const repository = new TicketRepository();
-const usecase = new ListTicketsUseCase(repository);
-const listTicketsController = new ListTicketsController(usecase);
-
-export { listTicketsController };
