966be313b3c4f7dd5aa88075cbdf9030356c2df1
theVieira <gabrielvieiraguimaraes4@gmail.com>
2024-08-19 16:26:01 -0400

    chore: add log on reopen

--- checkStatus.js
+++ checkStatus.js
@@ -1,59 +1,61 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const { config } = require("dotenv");
-const { sign } = require("jsonwebtoken");
+const { exec } = require('child_process')
+const { config } = require('dotenv')
+const { sign } = require('jsonwebtoken')
 
-config();
+config()
 
-const baseUrl = process.env.BASE_URL;
-const SECRET = process.env.SECRET_KEY ?? "";
-const USERNAME = process.env.USERNAME;
+const baseUrl = process.env.BASE_URL
+const SECRET = process.env.SECRET_KEY ?? ''
+const USERNAME = process.env.USERNAME
 
 const token = sign(
-  {
-    name: USERNAME,
-    status: "active",
-    permissions: {
-      admin: true,
-      create_ticket: true,
-      delete_ticket: true,
-    },
-  },
-  SECRET
-);
+	{
+		name: USERNAME,
+		status: 'active',
+		permissions: {
+			admin: true,
+			create_ticket: true,
+			delete_ticket: true,
+		},
+	},
+	SECRET
+)
 
 async function checkStatus() {
-  const res = await fetch(baseUrl + "/ticket/list/all", {
-    headers: {
-      authorization: `Bearer ${token}`,
-    },
-  });
+	const res = await fetch(baseUrl + '/ticket/listProgress/all', {
+		headers: {
+			authorization: `Bearer ${token}`,
+		},
+	})
 
-  const data = await res.json();
+	const data = await res.json()
 
-  const progress = data.filter((ticket) => ticket.status == "progress");
+	const now = new Date()
 
-  const now = new Date();
+	data.forEach(async (el) => {
+		const timeProgress = new Date(el.progress)
+		const timeDiff = now.getTime() - timeProgress.getTime()
+		const diffInHours = Math.floor(timeDiff / (1000 * 60 * 60))
+		if (diffInHours == 0) {
+			const res = await fetch(baseUrl + '/ticket/open', {
+				headers: {
+					authorization: `Bearer ${token}`,
+					'Content-Type': 'application/json',
+				},
+				method: 'PUT',
+				body: JSON.stringify({
+					id: el.id,
+				}),
+			})
 
-  progress.forEach(async (el) => {
-    const timeProgress = new Date(el.progress);
-    const timeDiff = now.getTime() - timeProgress.getTime();
-    const diffInHours = Math.floor(timeDiff / (1000 * 60 * 60));
-    if (diffInHours == 0) {
-      const res = await fetch(baseUrl + "/ticket/open", {
-        headers: {
-          authorization: `Bearer ${token}`,
-          "Content-Type": "application/json",
-        },
-        method: "PUT",
-        body: JSON.stringify({
-          id: el.id,
-        }),
-      });
+			const data = await res.json()
 
-      const data = await res.json();
-      console.log(data);
-    }
-  });
+			exec(
+				`echo '**Ticket time for finish expired** \nTicket reopen: \nId: ${data.id} \nClient: ${data.clientName} \nDescrição: ${data.description} \n' >> status.log`
+			)
+		}
+	})
 }
 
-setTimeout(() => checkStatus(), 1000 * 60 * 60 * 6);
+setTimeout(() => checkStatus(), 1000 * 10)
