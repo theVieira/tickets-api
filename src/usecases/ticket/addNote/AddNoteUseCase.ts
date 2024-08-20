1946428ba2f8b0547a71452690e2dff183d6a3fa
theVieira <gabrielvieiraguimaraes4@gmail.com>
2024-08-20 10:10:47 -0400

    fix: format string

--- src/usecases/ticket/addNote/AddNoteUseCase.ts
+++ src/usecases/ticket/addNote/AddNoteUseCase.ts
@@ -6,13 +6,27 @@ const SECRET = process.env.SECRET_KEY ?? ''
 export class AddNoteUseCase {
 	constructor(private ticketRepository: ITicketRepository) {}
 
-	async execute(id: string, note: string, token: string) {
+	async execute(id: string, note: string, techName: string, token: string) {
 		verify(token, SECRET)
+
 		if (note.length > 500) {
 			throw new Error('char limit over (max 500)')
 		}
 
-		const ticket = await this.ticketRepository.addNote(id, note)
-		return ticket
+		const formatNote = `Técnico: ${techName}\n=> ${note}\n`
+
+		const find = await this.ticketRepository.findById(id)
+
+		if (find.note == undefined) {
+			const ticket = await this.ticketRepository.addNote(id, formatNote)
+			console.log(ticket)
+			return ticket
+		} else {
+			const ticket = await this.ticketRepository.addNote(
+				id,
+				`${find.note}${formatNote}`
+			)
+			return ticket
+		}
 	}
 }
