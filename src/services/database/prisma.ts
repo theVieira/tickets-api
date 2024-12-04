9ab493465889fc8a349fa34673fdc61fa343d58b
theVieira <gabrielvieiraguimaraes4@gmail.com>
2024-12-04 15:08:22 -0400

    remove prisma.ticketNote

--- src/services/database/prisma.ts
+++ src/services/database/prisma.ts
@@ -5,6 +5,5 @@ const prisma = new PrismaClient()
 const client_gateway = prisma.client
 const tech_gateway = prisma.tech
 const ticket_gateway = prisma.ticket
-const note_gateway = prisma.ticketNote
 
-export { client_gateway, tech_gateway, ticket_gateway, note_gateway }
+export { client_gateway, tech_gateway, ticket_gateway }
